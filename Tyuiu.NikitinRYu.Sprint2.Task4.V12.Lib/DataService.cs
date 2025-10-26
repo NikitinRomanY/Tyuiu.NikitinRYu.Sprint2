00fc6463c821e6a3129b08fb0e83bcecf7ecbf1f
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-10-26 20:33:26 +0500

    Task4

--- Tyuiu.NikitinRYu.Sprint2.Task4.V12.Lib/DataService.cs
+++ Tyuiu.NikitinRYu.Sprint2.Task4.V12.Lib/DataService.cs
@@ -6,10 +6,15 @@ namespace Tyuiu.NikitinRYu.Sprint2.Task4.V12.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = Math.Sqrt(x) > 2 * y
-                ? Math.Pow(7 + 2 / (y * y), x)
-                : (3 * x * x - Math.Cos(y * y) + 10)
-                  / (y * y - Math.Pow(Math.Sin(x), 2) + 12);
+            //double z = Math.Sqrt(x) > 2 * y
+            //    ? Math.Pow(7 + 2 / (y * y), x)
+            //    : (3 * x * x - Math.Cos(y * y) + 10)
+            //      / (y * y - Math.Pow(Math.Sin(x), 2) + 12);
+
+            double z = Math.Sqrt(x) > 2.0 * y 
+                ? Math.Pow(7.0 + (2.0 / (y * y)), x) 
+                : ((3.0 * x * x - Math.Cos(y) * Math.Cos(y) + 10.0) 
+                / (y * y - Math.Sin(x * x) + 12.0));
 
             return Math.Round(z, 3);
         }
